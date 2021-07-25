901b4361c62b2d5e56aef5b57258a2108312162f
LowRonin <mg.mg.mghostrecon7@mail.ru>
2021-07-25 18:04:47 +0300

    Можно задание посложнее, и критики побольше?

--- src/main/java/HomeWorkApp/HomeWorkApp.java
+++ src/main/java/HomeWorkApp/HomeWorkApp.java
@@ -13,7 +13,7 @@ public class HomeWorkApp {
         System.out.println(leapYear(year));
     }
 
-
+//Задание 1
     public static boolean returnBool(int a, int b) {
         if (a + b > 10 && a + b < 20) {
             return true;
@@ -21,7 +21,7 @@ public class HomeWorkApp {
             return false;
         }
     }
-
+//Задание 2
     public static void returnPosOrNeg(int a) {
         if (a >= 0) {
             System.out.println("Число положительное");
@@ -31,7 +31,7 @@ public class HomeWorkApp {
         }
 
     }
-
+//Задание 3
     public static boolean returnBoolPosOrNeg(int b) {
         if (b >= 0) {
             return false;
@@ -39,13 +39,13 @@ public class HomeWorkApp {
             return true;
         }
     }
-
+//Задание 4
     public static void returnStringNumOfTimes(String x, int a) {
         for (int i = 0; i < a; i++) {
             System.out.println(x);
         }
     }
-
+//Задание 5
     public static boolean leapYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             return true;
